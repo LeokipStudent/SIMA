ac17672a6c2daf636308e3636c8c1629384ea18f
LeokipStudent <leoki201@student.liu.se>
2025-01-02 15:12:13 +0100

    Add files via upload

---
+++ BatVarnare/src/main.cpp
@@ -0,0 +1,37 @@
+/*
+  Project to send a warning if the boat is taking in water or the battery is dying.
+  Warnings sent using sms
+  1. Send sms
+  2. Check water sensor
+  3. Check battery voltage
+  4. Integrate everything
+  5. Add new phone number to the system via sms
+  6. Status updates upon receiving sms
+
+  Authors: Erik and Leonard
+
+*/
+
+
+
+
+
+
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
